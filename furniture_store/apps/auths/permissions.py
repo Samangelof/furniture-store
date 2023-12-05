49d32e4efe552d65eaa6de01a08bf156787b3d09
Markusha <87038045+Samangelof@users.noreply.github.com>
2023-12-05 22:30:28 +0300

    Delete furniture_store directory
    DEL

--- furniture_store/apps/auths/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import permissions
-
-class IsOwner(permissions.BasePermission):
-    """
-    Разрешение только владельцу объекта
-    """
-
-    def has_object_permission(self, request, view, obj):
-        return obj == request.user
